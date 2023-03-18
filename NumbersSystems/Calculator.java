8bf8746f6e4fb33de42ae8682d67858f77d1a617
Ibrahim Awny <67521813+hima12-awny@users.noreply.github.com>
2023-03-18 23:28:03 +0200

    Update Calculator.java

--- NumbersSystems/Calculator.java
+++ NumbersSystems/Calculator.java
@@ -3,7 +3,6 @@ package NumbersSystems;
 // Calculator  can perform basic operations like addition, subtraction, multiplication and division
 // on Two numbers even they are with different bases system.
 
-
 public class Calculator {
 
     Convertor convert = new Convertor();
@@ -19,7 +18,6 @@ public class Calculator {
             target_base is the base of the result number.
         */
 
-
         double n1 = convert.BaseToDecimal(num1, b1); // converts first number to decimal number
 
         double n2 = convert.BaseToDecimal(num2, b2); // converts second number to decimal number
@@ -27,7 +25,6 @@ public class Calculator {
         double result_num = 0.0;
 
         String polarity = "+"; // assume the result is positive (+)
-
 
         switch (ope) {
             case "+" -> result_num = n1 + n2;
@@ -41,7 +38,6 @@ public class Calculator {
                 if (n2 == 0)
                     throw new ArithmeticException("divide by zero");
                 result_num = n1 / n2;
-
             }
         }
 
